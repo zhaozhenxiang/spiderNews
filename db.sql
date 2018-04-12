58e63e8c032126ec608bef84fbb2f47298c7a280
赵祯祥 <zhaozhenxiang@everyoo.com>
2018-04-12 16:27:51 +0800

    修改数据库结构

--- db.sql
+++ db.sql
@@ -3,13 +3,18 @@
 DROP DATABASE IF EXISTS news;
 CREATE DATABASE news;
 USE news;
-CREATE TABLE news_all(
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	day VARCHAR(255) NOT NULL,
-	title VARCHAR(255) NOT NULL,
-	site VARCHAR(255) NOT NULL,
-	keywords VARCHAR(255),
-	type VARCHAR(255),
-	url VARCHAR(255) NOT NULL,
-	article TEXT
-) DEFAULT CHARSET=UTF8;
+DROP TABLE IF EXISTS `news_all`;
+CREATE TABLE `news_all` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `site` varchar(255) NOT NULL,
+  `keywords` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `url` varchar(255) NOT NULL,
+  `article` text,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `urlUnique` (`url`) USING HASH,
+  KEY `url` (`url`) USING HASH
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 DELAY_KEY_WRITE=1;
